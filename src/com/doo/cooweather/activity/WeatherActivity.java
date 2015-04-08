2186338c6c8f2960d4ea4e275ca2d59b9842d319
DuMingwei <960174331@qq.com>
2015-04-09 00:00:38 +0800

    增加后台自动更新天气的服务。

--- src/com/doo/cooweather/activity/WeatherActivity.java
+++ src/com/doo/cooweather/activity/WeatherActivity.java
@@ -1,6 +1,7 @@
 package com.doo.cooweather.activity;
 
 import com.doo.cooweather.R;
+import com.doo.cooweather.service.AutoUpdateService;
 import com.doo.cooweather.util.HttpCallbackListener;
 import com.doo.cooweather.util.HttpUtil;
 import com.doo.cooweather.util.Utility;
@@ -101,6 +102,8 @@ public class WeatherActivity extends Activity implements OnClickListener {
 		currentDateText.setText(prefs.getString("current_date", ""));
 		weatherInfoLayout.setVisibility(View.VISIBLE);
 		cityNameText.setVisibility(View.VISIBLE);
+		Intent intent = new Intent(this,AutoUpdateService.class);
+		startService(intent);
 	}
 
 	/**
---
+++ src/com/doo/cooweather/receiver/AutoUpdateReceiver.java
@@ -0,0 +1,17 @@
+package com.doo.cooweather.receiver;
+
+import com.doo.cooweather.service.AutoUpdateService;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AutoUpdateReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		Intent i = new Intent(context, AutoUpdateService.class);
+		context.startService(i);
+	}
+
+}
